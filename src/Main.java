721b45331b88927ee9c6fa2eb7d64d15f02a7c1f
koendaviduk <davidukj@mail.uc.edu>
2023-09-18 12:57:53 -0400

    finished

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+       int purchasePrice = 5;
+       double salesTax = purchasePrice * 0.05;
+       System.out.println("Your price was " + purchasePrice + " dollars " + "and your sales tax is " + salesTax + " cents");
+    }
+}
